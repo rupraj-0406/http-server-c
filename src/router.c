a75fc8338627667ed95cf2a2abd275589fca6fbf
rupraj-0406 <ruprajroy.r3@gmail.com>
2026-04-07 23:10:13 +0530

    adding DELETE, PATCH & HEAD method inside route

--- src/router.c
+++ src/router.c
@@ -2,9 +2,11 @@
 #include <string.h>
 #include <unistd.h>
 
-void route_request(int client_fd, HttpRequest *req) {
+void route_request(int client_fd, HttpRequest *req)
+{
 
-    if (strcmp(req->method, "GET") == 0 && strcmp(req->path, "/") == 0) {
+    if (strcmp(req->method, "GET") == 0 && strcmp(req->path, "/") == 0)
+    {
 
         char *response =
             "HTTP/1.1 200 OK\r\n"
@@ -14,7 +16,8 @@ void route_request(int client_fd, HttpRequest *req) {
         send(client_fd, response, strlen(response), 0);
     }
 
-    else if (strcmp(req->method, "GET") == 0 && strcmp(req->path, "/hello") == 0) {
+    else if (strcmp(req->method, "GET") == 0 && strcmp(req->path, "/hello") == 0)
+    {
 
         char *response =
             "HTTP/1.1 200 OK\r\n"
@@ -22,19 +25,54 @@ void route_request(int client_fd, HttpRequest *req) {
             "Hello route working!";
 
         send(client_fd, response, strlen(response), 0);
-    }else if (strcmp(req->method, "POST") == 0 && strcmp(req->path, "/data") == 0) {
+    }
+    else if (strcmp(req->method, "POST") == 0 && strcmp(req->path, "/data") == 0)
+    {
 
-    char response[4096];
+        char response[4096];
 
-    sprintf(response,
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: text/plain\r\n\r\n"
-        "Received data: %s", req->body);
+        sprintf(response,
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: text/plain\r\n\r\n"
+                "Received data: %s",
+                req->body);
 
-    send(client_fd, response, strlen(response), 0);
-}
+        send(client_fd, response, strlen(response), 0);
+    }
+    else if (strcmp(req->method, "DELETE") == 0 && strcmp(req->path, "/data") == 0)
+    {
 
-    else {
+        char *response =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n\r\n"
+            "Data deleted successfully";
+
+        send(client_fd, response, strlen(response), 0);
+    }
+    else if (strcmp(req->method, "PATCH") == 0 && strcmp(req->path, "/data") == 0)
+    {
+
+        char response[4096];
+
+        sprintf(response,
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: text/plain\r\n\r\n"
+                "Updated data: %s",
+                req->body);
+
+        send(client_fd, response, strlen(response), 0);
+    }
+    else if (strcmp(req->method, "HEAD") == 0)
+    {
+
+        char *response =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n\r\n";
+
+        send(client_fd, response, strlen(response), 0);
+    }
+    else
+    {
 
         char *response =
             "HTTP/1.1 404 Not Found\r\n"
